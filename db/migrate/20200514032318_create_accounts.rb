26d9d5d53d31aa62fc57ac0602c9bf70a03fe2f7
ncaudill27 <nelson.a.caudill@gmail.com>
2020-05-13 23:23:51 -0400

    Scaffold Accounts

---
+++ db/migrate/20200514032318_create_accounts.rb
@@ -0,0 +1,11 @@
+class CreateAccounts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :accounts do |t|
+      t.string :name
+      t.string :amount
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
